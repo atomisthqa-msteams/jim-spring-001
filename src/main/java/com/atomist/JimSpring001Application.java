42fa8699bfec345a2602333517a772589f7a128b
Jim Clark <slimslenderslacks@gmail.com>
2018-11-29 12:20:13 -0500

    Update JimSpring001Application.java

--- src/main/java/com/atomist/JimSpring001Application.java
+++ src/main/java/com/atomist/JimSpring001Application.java
@@ -8,6 +8,7 @@ public class JimSpring001Application {
 	public static void main(String[] args) {
 		org.springframework.boot.SpringApplication.run(JimSpring001Application.class,
 				args);
+		
 	}
 
 }
