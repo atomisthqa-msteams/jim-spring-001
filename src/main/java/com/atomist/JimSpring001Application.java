9dd7b6bc5026c7447eda08bbcbac63a7497a8d75
Jim Clark <slimslenderslacks@gmail.com>
2018-11-29 10:25:08 -0500

    Initial commit from Atomist

---
+++ src/main/java/com/atomist/JimSpring001Application.java
@@ -0,0 +1,12 @@
+package com.atomist;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JimSpring001Application {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(JimSpring001Application.class, args);
+	}
+
+}
